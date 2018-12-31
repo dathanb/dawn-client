84f293f049d5c6744ac6b4c973d1c7d1f6a000ae
Dathan Bennett <dathan@gmail.com>
2018-12-30 19:39:23 -0800

    Reformat editor-window.js

--- public/js/editor-window.js
+++ public/js/editor-window.js
@@ -1,65 +1,65 @@
 var normalizeLineEndings = function (str) {
-    return str.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
+  return str.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
 };
 
 var isReadOnly = function (cmd) {
-    return (cmd == "none" || !cmd);
+  return (cmd == "none" || !cmd);
 };
 
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
   var data = window.editorData;
-  if ( data == null ) {
-    data = { 'editorName' : 'Scratch', 'uploadCommand' : 'none', 'buffer' : '' };
+  if (data == null) {
+    data = {'editorName': 'Scratch', 'uploadCommand': 'none', 'buffer': ''};
   }
-  
+
   var uploadCommand = data.uploadCommand;
   var editorName = data.editorName;
   var buffer = data.buffer;
   var initialValue = buffer;
-  
+
   var cta = $('button.upload');
   var abort = $('button.abort');
   var basicEditor = $('div.editor textarea');
-  
-  if ( verbEditor != null )  {
+
+  if (verbEditor != null) {
     verbEditor.setValue(buffer);
     initialValue = verbEditor.getValue();
-  } else if ( basicEditor != null && basicEditor.length > 0 ) {
+  } else if (basicEditor != null && basicEditor.length > 0) {
     basicEditor.text(buffer);
     initialValue = basicEditor.val();
   }
-  
+
   // initial setup
   document.title = (isReadOnly(uploadCommand) ? 'Viewing ' : 'Editing ') + editorName;
   cta.html(uploadCommand);
-  
+
   // upload button
-  if ( cta && cta.length >= 1) {
-    cta.click(function() {
+  if (cta && cta.length >= 1) {
+    cta.click(function () {
       if (isReadOnly(uploadCommand)) {
         return;
       }
       //alert(editor.val());
       //var socket = window.uploadSocket;
       var socket = window.parentWindow.getSocket();
-      socket.emit('input', uploadCommand, function( state ) {
+      socket.emit('input', uploadCommand, function (state) {
         var uploadData = '';
-        if ( verbEditor != null ) {
+        if (verbEditor != null) {
           uploadData = verbEditor.getValue();
         } else {
           uploadData = basicEditor.val();
         }
-        socket.emit('input', uploadData + "\n.", function( state ) {
+        socket.emit('input', uploadData + "\n.", function (state) {
           initialValue = uploadData;
         });
       });
     });
   }
-  
+
   // abort or close button
-  if ( abort && abort.length >= 1) {
-    abort.on('click', function() {
+  if (abort && abort.length >= 1) {
+    abort.on('click', function () {
       var val = (verbEditor != null ? verbEditor.getValue() : basicEditor.val());
       if (val == initialValue || window.confirm('Abort editing and lose your changes?')) {
         window.close();
